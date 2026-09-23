812a8e9a9b19f8903af78aa030f66c52b89d5a51
Flmo1 <moiflo01@easv365.dk>
2026-09-23 12:55:06 +0200

    made NavBar and fixed some fonts

---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+
+// true = sound on, false = sound off
+const soundOn = ref(true)
+
+function toggleSound() {
+  soundOn.value = !soundOn.value
+}
+</script>
+
+<template>
+  <header class="bg-bark text-sand">
+    <div class="max-w-[1200px] mx-auto px-6">
+      <div class="flex items-center py-5">
+
+        <RouterLink to="/" class="font-display font-bold text-3xl uppercase mr-20">
+          Bioguessr
+        </RouterLink>
+
+      <RouterLink to="/" class="text-lg uppercase mr-10">Home</RouterLink>
+      <RouterLink to="/daily" class="text-lg uppercase mr-10">Daily</RouterLink>
+      <RouterLink to="/modes" class="text-lg uppercase mr-10">Game modes</RouterLink>
+
+      <RouterLink to="/login" class="ml-auto text-lg uppercase">Log in</RouterLink>
+
+     </div>
+    </div>
+  </header>
+</template>
